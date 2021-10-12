62a9eeb75b6f26012f3126bb1b1eeab9d4e51dfa
Romain Goguet <rom.goguet@laposte.net>
2021-10-13 00:29:29 +0200

    changed credential management

--- services/admin.js
+++ services/admin.js
@@ -1,9 +1,22 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../service.json');
+require('dotenv').config();
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(
+        {
+            "type": "service_account",
+            "project_id": process.env.FIRESTORE_PROJECTID,
+            "private_key_id": process.env.FIRESTORE_PRIVATEKEYID,
+            "private_key": process.env.FIRESTORE_PRIVATEKEY.replace(/\\n/g, '\n'),
+            "client_email": process.env.FIRESTORE_CLIENTMAIL,
+            "client_id": process.env.FIRESTORE_CLIENTID,
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": process.env.FIRESTORE_CLIENTX509
+        }
+    )
 });
 
 const db = admin.firestore();
